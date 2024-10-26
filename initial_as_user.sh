e250c450f5840fb45c667ec7f6506a131d925f85
rodrigo-goncalves <68075473+rodrigo-goncalves@users.noreply.github.com>
2024-10-26 22:24:18 +0100

    Adding a bit of docs to `initial_as_user` script

--- initial_as_user.sh
+++ initial_as_user.sh
@@ -1,8 +1,11 @@
 #!/bin/bash
 
 
-pipx install --include-deps ansible
+# This script is intended to be executed as a regular user with SUDO access.
+# The script execution would be triggered by the root user for running any commands
+#   that are supposed to run by a regular user
 
+pipx install --include-deps ansible
 pipx ensurepath
 
 ansible-playbook --connection=local 127.0.0.1, /tmp/linux-post-install-config/playbook.yml
