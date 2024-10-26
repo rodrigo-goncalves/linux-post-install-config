2a291bf0c5be42b14a523ab43614f1ccf39e9772
rodrigo <rodrigo@localhost.localdomain>
2024-10-26 13:10:45 +0100

    Using sudo for running certain commands

--- initial_setup.sh
+++ initial_setup.sh
@@ -1,28 +1,23 @@
 #/bin/bash
 
 
+apt update
+
 setup_pipx() {
-    apt update
-    apt install pipx -y
+    sudo apt install pipx -y
     pipx ensurepath
-    pipx ensurepath --global
-}
-
-setup_ansible() {
-    sudo su - ansible
-
-    [ "$USER" == 'ansible' ] && echo '' || exit 1
-
-    pipx install --include-deps ansible
 }
 
 setup_pipx
-setup_ansible
 
-git clone git@github.com:rodrigo-goncalves/linux-post-install-config.git
+pipx install --include-deps ansible
+
+git clone https://github.com/rodrigo-goncalves/linux-post-install-config.git
+
+cd linux-post-install-config
 
 ansible-playbook --connection=local 127.0.0.1, playbook.yml
 
 CURR=$(date +"%Y-%m-%dT%H:%M:%S%z")
 
-echo "${CURR}" >/etc/birth_certificate
+sudo echo "${CURR}" >/etc/birth_certificate
